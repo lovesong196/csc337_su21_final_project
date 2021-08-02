33b6eab5b407d4693662ddea538ebcb82c2355e8
lovesong196 <lovesong196@email.arizona.edu>
2021-08-01 22:05:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// TODO
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const port = 80;
+var app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/", (req, res)=> res.end("OK"));
+
+app.listen(port);
