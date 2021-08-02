b6cb599460948d7bca9bc333403c7e396ff0b4af
lovesong196 <lovesong196@email.arizona.edu>
2021-08-01 22:40:29 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser');
 const app = express()
 const port = 3000
+/*************************
+ *    MongoDB settings   *
+ *************************/
+const databaseURL = 'mongodb://localhost:27017/gomoku';
+mongoose.connect(databaseURL, {useNewUrlParser: true, useUnifiedTopology:true});
+// Schemas
+const Schema = mongoose.Schema
+const User = new Schema(
+    {
+        Username: String,
+        Password: String,
+        Status: String,
+        Room: Schema.Types.ObjectId
+    }
+);
 
+
+/*************************
+ *    Express Routes     *
+ *************************/
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
