49e92da27b3d4d2a8fc321be385eab9973800edb
Ferrari-559 <33665287+Ferrari-559@users.noreply.github.com>
2021-08-06 14:27:04 -0700

    Update game.js

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -1,1 +1,28 @@
-console.log('todo')
+
+function choose (){
+    $("map[name=currBoard] area").on('click', function (event) {
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        coords = this.coords.split('');
+        $.ajax({
+            type: "POST",
+            url: "/makeMove",
+            data: JSON.stringify(
+                {
+                    coords: coords[0] + ',' +coords[1]
+                }
+            ),
+            dataType: "text",
+            contentType:"application/json; charset=utf-8",
+            success: function (response) {
+                if(response == "SUCCESS"){
+                    console.log("SUCCESS")
+                } else {
+                    console.log("ERROR")
+                }
+            }
+        })
+        $('#'+this.id+'div').html('');
+    });
+};
+$("map[name=currBoard] area").on('click', function (event) {});
