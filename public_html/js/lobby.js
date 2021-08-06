dd6fbb9a77eaba0f042116430af14cec95a25459
lovesong196 <lovesong196@email.arizona.edu>
2021-08-06 12:59:40 -0700

    Update lobby

--- public_html/js/lobby.js
+++ public_html/js/lobby.js
@@ -12,3 +12,72 @@ function cancel(){
     $('#createOptionsWrapper').css('display', 'none')
     isFormHidden = true
 }
+function join(id){
+    for(room of rooms){
+        if(id == room._id){
+            if(room.password != null){
+                let password = prompt("Please enter password:")
+                if(password != room.password){
+                    alert("Wrong password")
+                    return
+                }
+            }
+        }
+    }
+    $.ajax({
+        type: "POST",
+        url: "/join/" + id,
+        data: null,
+        dataType: "text",
+        success: function (response) {
+            if(response == 'SUCCESS'){
+                window.location.href='/game/' + id
+            } else {
+                alert('Room is Full')
+            }
+        }
+    });
+}
+function create(){
+    $.ajax({
+        type: "POST",
+        url: "/create/",
+        data: JSON.stringify(
+            {
+                lobbyName: $('#lobbyName').val(),
+                isPrivate: $("#isPrivate").is(":checked"),
+                password: $("#password").val(),
+                color: $('#color').val()
+            }
+        ),
+        dataType: "text",
+        contentType:"application/json; charset=utf-8",
+        success: function (response) {
+            console.log(response)
+        }
+    });
+}
+var rooms
+function getRooms(results){
+    rooms = results
+    let list = $('#roomList')
+    const imageHtml = "<img src='Images/private.png' width='15`px'></img>"
+    for(room of rooms){
+        if(room.password != null){
+            let html = `<tr class='content'> <td class='room'>${room.name}` + imageHtml + `</td> <td> <input type="submit" class='button' id='join' value="Join" onclick='join("${room._id}");'></td></tr>`
+            list.html(list.html() + html)
+        } else {
+            let html = `<tr class='content'> <td class='room'>${room.name}</td> <td> <input type="submit" class='button' id='join' value="Join" onclick='join("${room._id}");'></td></tr>`
+            list.html(list.html() + html)
+        }
+        
+    }
+}
+$.ajax({
+    type: "GET",
+    url: "/rooms",
+    dataType: "json",
+    success: function (response) {
+        getRooms(response)
+    }
+});
