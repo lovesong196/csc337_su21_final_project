99cd75fceee06b79503a651ed6a7a5e310105814
Ferrari-559 <33665287+Ferrari-559@users.noreply.github.com>
2021-08-06 17:02:20 -0700

    Update code.js

--- public_html/js/code.js
+++ public_html/js/code.js
@@ -58,31 +58,9 @@ function loginUser(){
     })
 }
 
-function choose (){
-    $("map[name=currBoard] area").on('click', function (event) {
-        event.stopImmediatePropagation();
-        event.preventDefault();
-        coords = this.coords.split('');
-        $.ajax({
-            type: "POST",
-            url: "/makeMove",
-            data: JSON.stringify(
-                {
-                    coords: coords[0] + ',' +coords[1]
-                }
-            ),
-            dataType: "text",
-            contentType:"application/json; charset=utf-8",
-            success: function (response) {
-                if(response == "SUCCESS"){
-                    console.log("SUCCESS")
-                } else {
-                    console.log("ERROR")
-                }
-            }
-        })
-        $('#'+this.id+'div').html('');
-    });
-};
-$("map[name=currBoard] area").on('click', function (event) {});
+function goToHelp(){
+    document.location.href = 'help.html'
+}
+
+
 
